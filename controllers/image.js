a6019aab0eed396a5fd630c26f80c0bfab9d3dab
Betini Akarandut <betiniakarandut@gmail.com>
2022-04-23 17:53:07 +0100

    updating files

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+ const Clarifai = require('clarifai');
+
+ const app = new Clarifai.App({
+  apiKey: 'aeb9e21ae078455095bb09a726b06c65'
+});
+
+const handleApiCall = (req, res) => {
+   app.models
+     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+     .then(data => {
+        res.json(data);
+     })
+     .catch(err => res.status(400).json('unable to work with API'))
+}
+
+ const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+    	res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+};
