5ac5fa1256b795e0eb0016122fad393965c7055a
Betini Akarandut <betiniakarandut@gmail.com>
2022-04-25 01:46:26 +0100

    testing changes

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.Port || 3000, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
-})
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
