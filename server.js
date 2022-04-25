12ba3ad8527a8250eedbabbef84b1ef194e075ca
Betini Akarandut <betiniakarandut@gmail.com>
2022-04-25 18:01:07 +0100

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'ec2-3-218-171-44.compute-1.amazonaws.com',
     port : 5432,
     user : 'postgres',
     password : 'betini2024',
