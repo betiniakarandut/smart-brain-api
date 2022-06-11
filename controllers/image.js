c66a0d270b5ad621b515fb9f8cbbc4d796d07528
Betini Akarandut <betiniakarandut@gmail.com>
2022-06-11 22:06:41 +0100

    securing apiKey

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
  const Clarifai = require('clarifai');
 
  const app = new Clarifai.App({
-  apiKey: 'aeb9e21ae078455095bb09a726b06c65'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
